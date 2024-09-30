bcb9426047ac130ef615f97703578e68817ee1e4
ATOU11 <prince.atou@epitech.digital>
2024-09-30 14:32:38 +0200

    Create ex_06.js

---
+++ ex_06.js
@@ -0,0 +1,6 @@
+if (bananasCount == 0) {
+	displayThisText("oh no, there is no banana");
+} else if (bananasCount > 0) {
+	displayThisText("yummy!");
+} else { (bananasCount < 0);
+	displayThisText("Do I owo you bananas ?");
