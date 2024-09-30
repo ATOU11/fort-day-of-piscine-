8cf35044512c01ccfa3cab70db91dd64c9085f59
ATOU11 <prince.atou@epitech.digital>
2024-09-30 13:53:08 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,7 @@
+const firstname= "john";
+const lastname= "doe";
+const cardCode= "1111 2222 3333 4444";
+const expiryDateMonth= 21;
+const expiryDateYear= 2042;
+let computedBankInfo= firstname+ "," +lastname+ "," +cardCode+ "," +expiryDateMonth+ "/" +expiryDateYear
+displayInCard(computedBankInfo)
