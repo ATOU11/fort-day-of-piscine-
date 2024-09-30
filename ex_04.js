93bd3678f0807b9493c0c78b5f408495365e349a
ATOU11 <prince.atou@epitech.digital>
2024-09-30 12:40:51 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+displayThisText(typeof(chanter));
+displayThisText(typeof(true));
+displayThisText(typeof(3));
+displayThisText(typeof(53n))
+displayThisText(typeof Symbol(12345));
+displayThisText(typeof Object(0));
+displayThisText(typeof(displayThisText));
