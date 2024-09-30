85133af4997d8a0b7ab49801a357c7df38f27fac
ATOU11 <prince.atou@epitech.digital>
2024-09-30 16:52:54 +0200

    Create ex_09.js

---
+++ ex_09.js
@@ -0,0 +1,12 @@
+	displayThisText("Its type is integer")
+ 
+}
+else if (Number.isFinite(input_var)){ 
+ displayThisText("Its type is float")
+}
+else { 
+	displayThisText("Its type is " + typeof input_var)
+}
+if (input_var == 42) {
+	displayThisText("It is the meaning of life")
+}
