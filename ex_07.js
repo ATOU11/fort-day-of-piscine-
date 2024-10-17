9895e0f30e5c3bdf6fc6ba131a67bcb5995ea8b0
ATOU11 <prince.atou@epitech.digital>
2024-10-17 11:43:31 +0200

    ex_07.js

--- ex_07.js
+++ ex_07.js
@@ -1,5 +1,7 @@
 if (password == "forty-two") {
-  displayThisText("success")
-} else { 
-	displayThisText("Wrong password")
+    // Si la variable password est égale à "forty-two", afficher "Success" avec une majuscule initiale, conformément aux consignes.
+    displayThisText("Success");
+} else {
+    // Sinon, si le mot de passe est incorrect, afficher "Wrong password" avec la casse correcte.
+    displayThisText("Wrong password");
 }
