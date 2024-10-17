9d72ffdd7c7854ee79328e55b8ba642f84bdc51d
ATOU11 <prince.atou@epitech.digital>
2024-10-17 11:33:07 +0200

    ex_06.js

--- ex_06.js
+++ ex_06.js
@@ -1,6 +1,11 @@
 if (bananasCount == 0) {
-	displayThisText("oh no, there is no banana");
+    // Si bananasCount est égal à 0, affiche le texte correspondant.
+    displayThisText("Oh no, there is no banana");
 } else if (bananasCount > 0) {
-	displayThisText("yummy!");
-} else { (bananasCount < 0);
-	displayThisText("Do I owo you bananas ?");
+    // Si bananasCount est supérieur à 0, affiche "Yummy!".
+    displayThisText("Yummy!");
+} else {
+    // Si bananasCount est inférieur à 0, affiche "Do I owe you bananas?".
+    // Ici, on n'a pas besoin de vérifier avec "bananasCount < 0" car c'est déjà implicitement compris.
+    displayThisText("Do I owe you bananas?");
+}
