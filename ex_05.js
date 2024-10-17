835f4a69eb41181087927cad7eec5306ef23a436
ATOU11 <prince.atou@epitech.digital>
2024-10-17 11:22:30 +0200

    ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -1,7 +1,12 @@
-const firstname= "john";
-const lastname= "doe";
-const cardCode= "1111 2222 3333 4444";
-const expiryDateMonth= 21;
-const expiryDateYear= 2042;
-let computedBankInfo= firstname+ "," +lastname+ "," +cardCode+ "," +expiryDateMonth+ "/" +expiryDateYear
-displayInCard(computedBankInfo)
+// Déclaration des variables avec les valeurs spécifiées
+const firstname = "John";           // Chaîne de caractères pour le prénom
+const lastname = "Doe";             // Chaîne de caractères pour le nom
+const cardCode = "1111222233334444"; // Chaîne de caractères pour le code de la carte (sans espaces)
+const expiryDateMonth = 21;         // Entier pour le mois d'expiration
+const expiryDateYear = 2042;        // Entier pour l'année d'expiration
+
+// Construction de la chaîne computedBankInfo à partir des autres variables
+let computedBankInfo = firstname + "," + lastname + "," + cardCode + "," + expiryDateMonth + "/" + expiryDateYear;
+
+// Appel de la fonction displayInCard pour afficher le résultat
+displayInCard(computedBankInfo);    // Affiche les informations dans une carte (ou une autre interface selon ta fonction)
